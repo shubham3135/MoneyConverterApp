eb6824ae4d42ded05b8e205b9c280441d2cc0f40
Shubham <shubhamkumarwinner@gmail.com>
2021-05-22 11:47:34 +0530

    Hilt setup

---
+++ app/src/main/java/com/shubhamkumarwinner/moneyconverter/CurrencyApplication.kt
@@ -0,0 +1,8 @@
+package com.shubhamkumarwinner.moneyconverter
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CurrencyApplication: Application() {
+}
